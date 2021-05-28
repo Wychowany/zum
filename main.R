5edc58020ad6475de5cb5e5c39ebd2efec1fde25
olek <aleksander.nuszel@gmail.com>
2021-05-28 12:29:59 +0200

    rpart

--- main.R
+++ main.R
@@ -1,16 +1,32 @@
 library(rsample)      # data splitting 
 library(randomForest) # basic implementation
+library(caret)
+folds = 5
 
-set.seed(123)
-ames_split <- initial_split(AmesHousing::make_ames(), prop = .7)
-ames_train <- training(ames_split)
-ames_test  <- testing(ames_split)
+data_set = AmesHousing::make_ames()
+interval = nrow(data_set)/folds
 
-m1 <- randomForest(
-  formula = Sale_Price ~ .,
-  data    = ames_train
-)
+diff_pred = as.null()
 
-pred_randomForest <- predict(m1, ames_test)
 
-error = sqrt(sum((ames_test['Sale_Price'] - pred_randomForest)^2))
+for (i in 0:(folds-1)) {
+  beginIndex = i * interval
+  endIndex = (i+1) * interval - 1
+  validation = data_set[beginIndex:endIndex,]
+  left = data_set[0:beginIndex,]
+  right = data_set[(endIndex+1):(folds*interval),]
+  train_set = rbind(left,right)
+  model <- randomForest(
+    formula = Sale_Price ~ .,
+    ntree = 300,
+    data    = train_set
+  )
+  pred_randomForest <- predict(model, validation)
+  if (is.null(diff_pred)) {
+    diff_pred = (validation['Sale_Price'] - pred_randomForest)^2
+  } else {
+    diff_pred = rbind(diff_pred,(validation['Sale_Price'] - pred_randomForest)^2)
+  }
+}
+summary(diff_pred)
+
---
+++ rpart_test.R
@@ -0,0 +1,46 @@
+library(rpart)       # performing regression trees
+library(rpart.plot)  # plotting regression trees
+library(caret)
+library(sample)
+
+folds = 5
+ntrees = 300
+single_tree_data_factor = 0.66
+
+data_set = AmesHousing::make_ames()
+interval = nrow(data_set)/folds
+single_tree_data_size = as.integer(single_tree_data_factor * nrow(data_set))
+diff_pred = as.null()
+
+for (i in 0:(folds-1)) {
+  forest_pred = as.null()
+  beginIndex = i * interval
+  endIndex = (i+1) * interval - 1
+  validation = data_set[beginIndex:endIndex,]
+  left = data_set[0:beginIndex,]
+  right = data_set[(endIndex+1):(folds*interval),]
+  train_set = rbind(left,right)
+  
+  for (j in 0:(ntrees-1)) {
+    single_tree_train = train_set[sample(nrow(train_set),single_tree_data_size),]
+    model <- rpart(
+      formula = Sale_Price ~ .,
+      data    = single_tree_train,
+      method  = "anova"
+    )
+    tree_pred = predict(model, validation)
+    if (is.null(forest_pred)) {
+      forest_pred = tree_pred
+    } else {
+      forest_pred = forest_pred + tree_pred
+    }
+  }
+  forest_pred = forest_pred/ntrees;
+  
+  if (is.null(diff_pred)) {
+    diff_pred = (validation['Sale_Price'] - forest_pred)^2
+  } else {
+    diff_pred = rbind(diff_pred,(validation['Sale_Price'] - forest_pred)^2)
+  }
+}
+summary(diff_pred)
